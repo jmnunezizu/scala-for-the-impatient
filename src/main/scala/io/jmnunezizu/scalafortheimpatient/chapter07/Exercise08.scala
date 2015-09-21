78f1b2ae2ff615431e9368bfc0cb1f795545e1a4
Jose Maria Nunez Izu <jmnunezizu@gmail.com>
2015-09-21 13:20:12 +0100

    Add chapter 7 exercise 8.

---
+++ src/main/scala/io/jmnunezizu/scalafortheimpatient/chapter07/Exercise08.scala
@@ -0,0 +1,21 @@
+package io.jmnunezizu.scalafortheimpatient.chapter07
+
+/**
+ * Chapter 7 - Exercise 8.
+ *
+ * What is the effect of
+ *
+ * {code}
+ * import java._
+ * import javax._
+ * {code}
+ */
+object Exercise08 extends App {
+
+  println(s"""
+    It brings everything under java and javax into scope. It might be a better
+    idea to just import the needed subpackages/classes.
+    Importing everything can also lead to name clashes.
+  """)
+
+}
