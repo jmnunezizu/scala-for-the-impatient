c9d3c3d0291ff8e3db68eba4d68d369b83a8f02f
Jose Maria Nunez Izu <jmnunezizu@gmail.com>
2015-09-21 12:06:17 +0100

    Add chapter 7 exercise 1.

---
+++ src/main/scala/io/jmnunezizu/scalafortheimpatient/chapter07/Exercise01.scala
@@ -0,0 +1,47 @@
+package io.jmnunezizu.scalafortheimpatient.chapter07
+
+/**
+ * Chapter 7 - Exercise 1.
+ *
+ * Write an example program to demonstrate that
+ *
+ * {code}
+ * package com.horstmann.impatient
+ * {code}
+ *
+ * is not the same as
+ *
+ * {code}
+ * package com
+ * package horstmann
+ * package impatient
+ * {code}
+ */
+object Exercise01 extends App {
+
+}
+
+package com.horstmann {
+  object Cay {
+    val name: String = "Cay Horstmann"
+  }
+}
+
+package com {
+  package horstmann {
+    package impatient {
+      class A {
+        println(Cay.name) // Cay can be accessed
+      }
+    }
+  }
+}
+
+package com.horstmann.impatient {
+
+  import io.jmnunezizu.scalafortheimpatient.chapter07.com.horstmann.Cay
+
+  class B {
+    println(Cay.name) // Cay can be accessed by importing the class
+  }
+}
