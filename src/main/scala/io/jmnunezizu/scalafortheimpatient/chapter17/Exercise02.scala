14a97450abbcf30a839819a0c15a2981bdcd3f4a
Jose Maria Nunez Izu <jmnunezizu@gmail.com>
2015-09-28 15:32:13 +0100

    Add chapter 17 exercise 2.

---
+++ src/main/scala/io/jmnunezizu/scalafortheimpatient/chapter17/Exercise02.scala
@@ -0,0 +1,22 @@
+package io.jmnunezizu.scalafortheimpatient.chapter17
+
+/**
+ * Chapter 17 - Exercise 2.
+ *
+ * Define a mutable class Pair[T] with a method swap that swaps the components
+ * of the pair.
+ */
+object Exercise02 extends App {
+
+  class Pair[T](val a: T, val b: T) {
+    def swap(): Pair[T] = new Pair(b, a)
+    override def toString(): String = s"($a,$b)"
+  }
+
+  val p1: Pair[Int] = new Pair(29, 92)
+  val p2 = p1.swap()
+
+  assert(p1.toString == "(29,92)")
+  assert(p2.toString == "(92,29)")
+
+}
