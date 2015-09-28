373f4f5616c51e08c26c2aca6968c59d111eeb05
Jose Maria Nunez Izu <jmnunezizu@gmail.com>
2015-09-28 13:06:32 +0100

    Add chapter 14 exercise 9.

--- src/main/scala/io/jmnunezizu/scalafortheimpatient/chapter14/Exercise09.scala
+++ src/main/scala/io/jmnunezizu/scalafortheimpatient/chapter14/Exercise09.scala
@@ -11,11 +11,13 @@ object Exercise09 extends App {
   def f1(l: List[Option[Int]]): Int = l.flatMap(x => x).sum
   def f2(l: List[Option[Int]]): Int = l.flatten.sum
   def f3(l: List[Option[Int]]): Int = (for (Some(n) <- l) yield n).sum
+  def f4(l: List[Option[Int]]): Int = l.foldLeft(0)((total, n) => total + n.getOrElse(0))
 
   val l = List(None, Some(5), Some(6), None, None)
 
   assert(f1(l) == 11)
   assert(f2(l) == 11)
   assert(f3(l) == 11)
+  assert(f4(l) == 11)
 
 }
